692817666843a99922e34b0608b70dcfc86c22db
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-01-29 08:53:57 -0800

    added parametrize functions

--- test_wallet.py
+++ test_wallet.py
@@ -15,6 +15,20 @@ def empty_wallet():
 def wallet():
 	return Wallet(20)
 
+@pytest.fixture
+def my_wallet():
+	return Wallet()
+
+@pytest.mark.parametrize("earned,spent,expected", [
+	(30, 10, 20),
+	(20, 2, 18),
+])
+
+def test_transactions(my_wallet, earned, spent, expected):
+	my_wallet.add_cash(earned)
+	my_wallet.spend_cash(spent)
+	assert my_wallet.balance == expected
+
 def test_default_initial_amount(empty_wallet):
 	assert empty_wallet.balance == 0
 
